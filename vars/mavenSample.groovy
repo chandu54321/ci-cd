783730e8dadfdb26e6cc5bb810593b23ebdc5680
chandu54321 <kasinenichandu@gmail.com>
2024-08-20 16:24:09 +0530

    dd

--- vars/mavenSample.groovy
+++ vars/mavenSample.groovy
@@ -6,8 +6,10 @@ pipeine{
     }
     stages {
         stage ('git') {
-            steps git url: 'https://github.com/spring-projects/spring-petclinic.git',
+           steps {
+            git url: 'https://github.com/spring-projects/spring-petclinic.git',
             branch: 'main'
+           }
         }
         stage ('build') {
             sh 'mvn clean package'
